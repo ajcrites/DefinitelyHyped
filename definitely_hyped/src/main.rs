9ae3a9f11b22d986bef3561af244ef308327c69d
Andrew Crites <acrites@mobiquityinc.com>
2016-11-11 13:49:14 -0500

    Uploading parsed @types json to S3

--- definitely_hyped/src/main.rs
+++ definitely_hyped/src/main.rs
@@ -1,8 +1,13 @@
 extern crate json;
+extern crate rusoto;
 
-use std::process::Command;
+use std::process::{Command, exit};
 use std::fs::read_dir;
 use std::path::Path;
+use std::env;
+
+use rusoto::{ChainProvider, ProfileProvider, Region};
+use rusoto::s3::{S3Client, PutObjectRequest};
 
 fn main() {
     const REPO_PATH: &'static str = "DefinitelyTyped";
@@ -30,5 +35,59 @@ fn main() {
     let types_names: Vec<_> = paths.iter()
         .map(|path| path.to_str().unwrap().replace(&format!("{}/", REPO_PATH), ""))
         .collect();
-    println!("{}", json::stringify(types_names));
+
+    let profile = match env::var("AWS_PROFILE") {
+        Ok(val) => val.to_string(),
+        Err(_) => "default".to_string(),
+    };
+
+    let region = match env::var("AWS_REGION") {
+        Ok(val) => val.to_string(),
+        Err(_) => {
+            println!("{}", "You must provide an AWS Region");
+            exit(1);
+        }
+    };
+
+    let bucket = match env::var("BUCKET") {
+        Ok(val) => val.to_string(),
+        Err(_) => {
+            println!("{}", "You must provide the bucket to upload to");
+            exit(1);
+        }
+    };
+
+    let mut profile_provider = ProfileProvider::new().unwrap();
+    profile_provider.set_profile(profile);
+    let chain_provider = ChainProvider::with_profile_provider(profile_provider);
+
+    let s3_client = S3Client::new(chain_provider, region.parse::<Region>().unwrap());
+    s3_client.put_object(&PutObjectRequest {
+        bucket: bucket,
+        key: "@types.json".to_string(),
+        body: Some(json::stringify(types_names).as_bytes()),
+
+        request_payer: None,
+        content_encoding: None,
+        storage_class: None,
+        grant_read_acp: None,
+        server_side_encryption: None,
+        ssekms_key_id: None,
+        content_disposition: None,
+        metadata: None,
+        sse_customer_key: None,
+        website_redirect_location: None,
+        expires: None,
+        cache_control: None,
+        content_length: None,
+        grant_read: None,
+        grant_write_acp: None,
+        acl: None,
+        grant_full_control: None,
+        sse_customer_algorithm: None,
+        content_type: None,
+        content_language: None,
+        content_md5: None,
+        sse_customer_key_md5: None,
+    }).expect("could not upload");
 }
